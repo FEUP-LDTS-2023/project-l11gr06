f90f19ccc6036f7a8d4771d4c8950f313476ba10
Duarte Marques <up202204973@up.pt>
2023-11-29 11:41:44 +0000

    Gradlew Run (fixed)

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int lives = 3;
+        while (lives > 0) {
+            new Game(65, 20).run();
+            lives--;
+        }
+    }
+}
