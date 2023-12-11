a4031849ab66468ee04b3f2347b24380b039a134
martacruz212 <up202205028@up.pt>
2023-12-11 16:46:31 +0000

    Help Viewer and About Viewer

--- src/main/java/com/aor/supermario/viewer/HelpViewer.java
+++ src/main/java/com/aor/supermario/viewer/HelpViewer.java
@@ -12,22 +12,15 @@ public class HelpViewer extends Viewer<Help> {
 
     @Override
     public void drawElements(GUI gui) {
-        gui.drawText(new Position(24, 2), "GAME INSTRUCTIONS ", "#FFFFFF");
-        gui.drawText(new Position(15, 3), "     ", "#FFFFFF");
-        gui.drawText(new Position(15, 4), "     ", "#FFFFFF");
-        gui.drawText(new Position(15, 5), "PRESS THE RIGHT OR LEFT ARRO@ TO RUN", "#FFFFFF");
-        gui.drawText(new Position(15, 6), "     ", "#FFFFFF");
-        gui.drawText(new Position(15, 7), "PRESS THE UP ARRO@ TO JUMP", "#FFFFFF");
-        gui.drawText(new Position(15, 8), "     ", "#FFFFFF");
-        gui.drawText(new Position(15, 9), "JUMP ON TOP OF THE ENEMIES TO KILL THEM", "#FFFFFF");
-        gui.drawText(new Position(15, 10), "     ", "#FFFFFF");
-        gui.drawText(new Position(15, 11), "IF YOU BUMP INTO THEM YOU DIE", "#FFFFFF");
-        gui.drawText(new Position(15, 12), "     ", "#FFFFFF");
-        gui.drawText(new Position(15, 13), "AFTER 3 LIVES YOU LOSE", "#FFFFFF");
-        gui.drawText(new Position(15, 14), "     ", "#FFFFFF");
-        gui.drawText(new Position(15, 15), "REACH THE FINAL FLAG TO @IN THE GAME", "#FFFFFF");
+        gui.drawText(new Position(20, 2), "GAME INSTRUCTIONS ", "#FFFFFF");
+        gui.drawText(new Position(2, 5), "PRESS THE RIGHT OR LEFT ARRO@ TO RUN AND UP ARRO@ TO JUMP", "#FFFFFF");
+        gui.drawText(new Position(2, 7), "PRESS X TO DO A FRONT JUMP AND Z TO DO A BACK JUMP", "#FFFFFF");
+        gui.drawText(new Position(2, 9), "JUMP ON TOP OF THE ENEMIES TO KILL THEM", "#FFFFFF");
+        gui.drawText(new Position(2, 11), "IF YOU BUMP INTO ONE OF THE MONSTERS YOU DIE", "#FFFFFF");
+        gui.drawText(new Position(2, 13), "IF YOU ARE DONE PLAYING PRESS Q TO RETURN TO MENU", "#FFFFFF");
+        gui.drawText(new Position(2, 15), "REACH THE FINAL FLAG TO @IN THE GAME", "#FFFFFF");
 
         for (int i = 0; i < getModel().getNumberEntries(); i++)
-            gui.drawText( new Position(24, 18 + i), getModel().getEntry(i), getModel().isSelected(i) ? "#FF0000" : "#FFFFFF");
+            gui.drawText( new Position(21, 18 + i), getModel().getEntry(i), getModel().isSelected(i) ? "#FF0000" : "#FFFFFF");
     }
 }
