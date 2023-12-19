1959be58941db735c99cf55b5b5e93d2eadb1412
martacruz212 <up202205028@up.pt>
2023-12-19 22:13:44 +0000

    Some elements tests

--- src/main/java/com/aor/supermario/model/elements/RedMushroom.java
+++ src/main/java/com/aor/supermario/model/elements/RedMushroom.java
@@ -4,15 +4,8 @@ import com.aor.supermario.model.Position;
 
 public class RedMushroom extends Element {
 
-    public RedMushroom(Position p) {
-        super(p);
-    }
-
     public RedMushroom(int x, int y) {
         super(x, y);
     }
-    public void moveMapLeft() {
-        Position p=new Position(getPosition().getX()-1, getPosition().getY());
-        setPosition(p);
-    }
+
 }
