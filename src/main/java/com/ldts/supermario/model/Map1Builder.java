e8b4c2899345009a7141d40143c0a27d220dd36a
Duarte Marques <up202204973@up.pt>
2023-12-23 16:00:56 +0000

    Update

--- src/main/java/com/ldts/supermario/model/Map1Builder.java
+++ src/main/java/com/ldts/supermario/model/Map1Builder.java
@@ -216,7 +216,7 @@ public class Map1Builder extends MapBuilder{
     {
         List <Monster> monsters = new ArrayList<>();
         monsters.add(new Turtle(25, height - 4));
-        monsters.add(new BrownMushroom(15, height - 4)); //42
+        monsters.add(new BrownMushroom(42, height - 4));
         monsters.add(new BrownMushroom(53, height - 4));
         monsters.add(new BrownMushroom(55, height - 4));
         monsters.add(new BrownMushroom(83, height - 13));
